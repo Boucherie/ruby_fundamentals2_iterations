e77d3112530132937b630b9f1d16baafdc0e0eea
Emily Boucher <emi.boucher@gmail.com>
2017-05-16 14:19:51 -0400

    Ex1 No1 arrays

---
+++ exercises.rb
@@ -0,0 +1,5 @@
+colors = ["blue", "green", "purple", "red", "grey"]
+gen_age = [31, 33, 35, 41]
+coin_flip_heads = [true, true, true, true, false]
+performing_artists = ["Arcade Fire", "Metric", "July Talk"]
+colors = [:blue, :green, :purple, :red, :grey]
