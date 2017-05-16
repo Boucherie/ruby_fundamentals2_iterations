04982d8f03cfa0c1ae4b8b37a087585142890a93
Emily Boucher <emi.boucher@gmail.com>
2017-05-16 14:24:11 -0400

    Actually ex0 because indices, apparently

--- exercises.rb
+++ exercises.rb
@@ -3,3 +3,6 @@ gen_age = [31, 33, 35, 41]
 coin_flip_heads = [true, true, true, true, false]
 performing_artists = ["Arcade Fire", "Metric", "July Talk"]
 colors = [:blue, :green, :purple, :red, :grey]
+
+
+definitions = { }
